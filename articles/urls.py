02711b3f03de70c30998abc1381ce74857c1757e
SungSuSon <thstjdtn05@gmail.com>
2023-04-18 22:29:21 +0900

    feat/articles app view change CBV

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.articleAPI,name="index"),
-    path('<int:article_id>/',views.articleDetailAPI,name = "article_view")
+    path('',views.ArticleAPI.as_view(),name="index"),
+    path('<int:article_id>/',views.ArticleDetail.as_view(),name = "article_view")
 ]
